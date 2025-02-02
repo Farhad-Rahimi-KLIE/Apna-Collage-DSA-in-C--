99b407a1c131f1867c597d0d61037a6d6b9c8da2
Farhad Rahimi Klie <farhadrahimiklie786@gmail.com>
2025-02-02 11:14:18 +0430

    complate the Character Array and String

---
+++ 23 Character Array and String/main.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    // 1 - Characters Array - as known c string
+
+    // Character Array convert in single string in RAM of computer 
+    // char str[] = {'a','b','c','d','\0'}; // \0 is terminate the whole characters
+    // cout<<str<<endl;
+    // cout<<str[4]<<endl;
+
+    // char str[14];
+    // cout<<"enter element in str";
+    // cin.getline(str, 14);
+
+    // // cout<<"output is this : "<<str<<endl;
+
+    // for (auto el : str)
+    // {
+    //     cout<<el<<" ";
+    // }
+    // cout<<endl;
+
+
+
+
+    
+    // 2 - String
+    // string str = "farhad rahimi klie"; // string dynamic and Runtime resize and store data in continguous manner
+    // string str2 = "jan";
+    // string str3 = str+str2; // combine all string together as known contatenations
+    // cout<<str3<<endl;
+    // cout<<str.length()<<endl;
+
+    // string str;
+
+    // getline(cin, str);
+
+    // cout<<"output : "<<str<<endl;
+
+
+    // run string from loops
+
+    // string str = "hello World";
+    // for (int i = 0; i < str.length(); i++)
+    // {
+    // cout<<str[i]<<" ";
+    // }
+    // cout<<endl;
+    return 0;
+}
