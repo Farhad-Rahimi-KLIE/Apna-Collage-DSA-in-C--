c77b48e7f74658a30c9ec3c03d6ff2cf8800d552
Farhad Rahimi Klie <farhadrahimiklie786@gmail.com>
2025-03-11 16:29:53 +0430

    2d array is completed

--- 25 leetcode Remove Occurrences/main.cpp
+++ 25 leetcode Remove Occurrences/main.cpp
@@ -3,14 +3,6 @@
 
 using namespace std;
 
-bool isAlphaNum(char ch){
-    if ((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z'))
-    {
-        return true;
-    }
-    return false;
-}
-
 string RemoveOccurences(string s, string part){
     while (s.length() > 0 && s.find(part) < s.length())
     {
--- Practice.cpp
+++ Practice.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-    
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6,87,65},{7,8,9}};
+    //  rows = matrix.size();
+    //  cols = matrix[i].size();
+     for (int i = 0; i < matrix.size(); i++)
+     {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+     }
     return 0;
 }
